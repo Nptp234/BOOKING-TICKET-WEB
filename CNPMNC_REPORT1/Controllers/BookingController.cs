b048ae429b1651dd33105d3b85750561c8d6680a
ASUS <buihuyphuoc123@gmail.com>
2023-10-23 14:16:40 +0700

    Chọn ghế (có sử dụng db)
    Remake phần Chọn ghế nhưng có bao gồm sử dụng database để hiển thị chính xác hơn

--- CNPMNC_REPORT1/Controllers/BookingController.cs
+++ CNPMNC_REPORT1/Controllers/BookingController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -47,11 +48,61 @@ namespace CNPMNC_REPORT1.Controllers
             var data = new { List2D = list2D, List3D = list3D };
             return Json(data);
         }
+     
+        public ActionResult ChonGhe(string Type, string Date = "", string Time = "7:00:00", string MaPhim = "1", string stt = "1")
+        {
+            if (Date == null || Date == "")
+                Date = DateTime.Now.ToString("dd/MM/yyyy");
+            //LẤY DANH SÁCH CÁC GHẾ THƯỜNG VÀ VIP CÓ SẴN
+            ViewBag.getChair = db.getData($"SELECT LICHCHIEU.*, PHONGCHIEU.SLGheThuong, PHONGCHIEU.SLGheVIP, XUATCHIEU.GioXC, LOAIPC.TenLPC " +
+                                            $"FROM LICHCHIEU, XUATCHIEU, PHIM, PHONGCHIEU, LOAIPC " +
+                                            $"WHERE PHONGCHIEU.MaPC = LICHCHIEU.MaPC " +
+                                            $"AND PHIM.MaPhim = LICHCHIEU.MaPhim " +
+                                            $"AND LICHCHIEU.CaXC = XUATCHIEU.CaXC " +
+                                            $"AND LOAIPC.MaLPC = PHONGCHIEU.MaLPC " +
+                                            $"AND LOAIPC.TenLPC = '{Type}'" +
+                                            $"AND LICHCHIEU.NgayLC = '{Date}' " +
+                                            $"AND LICHCHIEU.MaPhim = {MaPhim} " +
+                                            $"AND XUATCHIEU.GioXC = '{Time}'");
+            ViewBag.getChair_STT = ViewBag.getChair[int.Parse(stt) - 1];
 
-        public ActionResult ChonGhe()
+            //LẤY DANH SÁCH GHẾ ĐÃ ĐƯỢC ĐẶT
+            ViewBag.ListPickedChair = db.getData($"SELECT VE_GHE.* FROM VEPHIM, VE_GHE WHERE VEPHIM.MaLC = {ViewBag.getChair_STT[0]} AND VEPHIM.MaVe = VE_GHE.MaVe");
+
+            //Tạo ViewBag để gửi dữ liệu cho những lần sau
+            ViewBag.Page = stt;
+            ViewBag.MaxPage = ViewBag.getChair.Count.ToString();
+            ViewBag.MinPage = 1.ToString();
+            ViewBag.Type = Type;
+            return View();
+        }
+        public JsonResult updateViewPrice(string listloaighe, string maphim = "1")
         {
             
-            return View();
+            double totalPrice = 0;
+            if (listloaighe != "")
+            {
+                string[] listLoaiGhe_Array = listloaighe.Split(' ');
+                foreach (string i in listLoaiGhe_Array)
+                {
+                    ViewBag.getFilm = db.getData($"SELECT* FROM PHIM WHERE MaPhim = {maphim}");
+                    if (ViewBag.getFilm != null)
+                    {
+                        string getString = ViewBag.getFilm[0][9];
+                        if (i.IndexOf("VIP", StringComparison.OrdinalIgnoreCase) >= 0)
+                        {
+                            totalPrice += Convert.ToDouble(getString) + 20000;
+                        }
+                        else
+                        {
+                            totalPrice += Convert.ToDouble(getString);
+                        }
+                    }
+                }
+            }
+            
+            var data = new { totalPrice = totalPrice};
+            return Json(data);
         }
     }
 }
